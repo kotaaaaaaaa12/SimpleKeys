5dc7af8e1e9a6bbc147432939610264f33949cbb
kotaaaaaaaa12 <kotarohigashi12@gmail.com>
2026-08-14 05:29:52 +0900

    Clean up temp python scripts

--- add_crop_vc.py
+++
@@ -1,5 +0,0 @@
-from pbxproj import XcodeProject
-
-project = XcodeProject.load('SimpleKeys.xcodeproj/project.pbxproj')
-project.add_file('SimpleKeys/ImageCropViewController.swift', target_name='SimpleKeys', force=False)
-project.save()
--- add_font_manager.py
+++
@@ -1,6 +0,0 @@
-from pbxproj import XcodeProject
-
-project = XcodeProject.load('SimpleKeys.xcodeproj/project.pbxproj')
-project.add_file('SimpleKeys/CustomFontManager.swift', target_name='SimpleKeys', force=False)
-project.add_file('SimpleKeys/CustomFontManager.swift', target_name='KeyboardExtension', force=False)
-project.save()
